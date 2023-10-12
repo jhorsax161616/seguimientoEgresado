08689319ab71d5aa252b4ad43c21df7f6a8b2c8d
Jhordan Sax <71660993@continental.edu.pe>
2023-10-12 17:00:33 -0500

    Personalizando index de administracion de egresados

--- resources/views/egresados/index.blade.php
+++ resources/views/egresados/index.blade.php
@@ -3,30 +3,30 @@
     
 @section('content_header')
     <h1>Bienvenido a la seccion para administrar egresados</h1>
-    <br>
-    <div style="text-align: right;">
-        <a href="{{route('egresados.create')}}" class="btn btn-dark btn-sm btn-inline">
-            <i class="fa fa-briefcase"></i> Nuevo Egresado
-        </a>
-    </div>
 @stop
 @section('content')
-<table>
+<div style="text-align: right;">
+    <a href="{{route('egresados.create')}}" class="btn btn-dark btn-sm btn-inline">
+        <i class="fa fa-briefcase"></i> Nuevo Egresado
+    </a>
+</div>
+<br>
+<table class="table table-bordered table-hover dataTable dtr-inline collapsed">
     <thead>
         <tr>
+            <th>Revisar</th>
             <th>Nombre</th>
             <th>Especializacion</th>
-            <th>Revisar</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($egresados as $egresado)
             <tr>
+                <td>
+                    <a href="{{route('egresados.show', $egresado->id)}}" class="btn btn-block btn-light">ver</a>
+                </td>
                 <td>{{$egresado->nombre}}</td>
                 <td>{{$egresado->especializacion}}</td>
-                <td>
-                    <a href="{{route('egresados.show', $egresado->id)}}">ver</a>
-                </td>
             </tr>
 
         @endforeach
